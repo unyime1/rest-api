e7a9c02942146ba79d727b922b5995ff2a89cd37
Unyime Etim <lordunyime@gmail.com>
2020-11-28 15:43:08 +0100

    completed API

--- snippets/views.py
+++ snippets/views.py
@@ -58,6 +58,19 @@ from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
